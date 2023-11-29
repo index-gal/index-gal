19ff6904e8b59885758d2b1f9f422db22de33622
Oscar Otero <oom@oscarotero.com>
2023-11-29 21:16:43 +0100

    url

--- _config.ts
+++ _config.ts
@@ -5,6 +5,7 @@ import favicon from "lume/plugins/favicon.ts";
 import svgo from "lume/plugins/svgo.ts";
 import inline from "lume/plugins/inline.ts";
 import metas from "lume/plugins/metas.ts";
+import basepath from "lume/plugins/base_path.ts";
 
 const site = lume({
   location: new URL("https://index.gal"),
@@ -14,6 +15,7 @@ const site = lume({
   .use(svgo())
   .use(inline())
   .use(metas())
+  .use(basepath())
   .use(favicon({
     input: "index.gal.svg",
   }))
