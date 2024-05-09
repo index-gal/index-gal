85b62ceaee9882c75922162b723cca013eb2f498
Oscar Otero <oom@oscarotero.com>
2024-05-09 12:22:24 +0200

    xerar o link do calendario automaticamente

--- _config.ts
+++ _config.ts
@@ -6,7 +6,7 @@ import inline from "lume/plugins/inline.ts";
 import metas from "lume/plugins/metas.ts";
 import basepath from "lume/plugins/base_path.ts";
 import date from "lume/plugins/date.ts";
-
+import { google } from "npm:calendar-link@2.6.0";
 import gl from "npm:date-fns@2.30.0/locale/gl/index.js";
 
 const site = lume({
@@ -26,6 +26,7 @@ const site = lume({
   .copy("index.gal.svg")
   .copy("CNAME")
   .copy([".js"])
+  .filter("google_calendar", google)
   .remoteFile(
     "lite-yt-embed.js",
     "https://cdn.jsdelivr.net/npm/lite-youtube-embed@0.3.1/src/lite-yt-embed.js",
